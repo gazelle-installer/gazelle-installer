e41413472561b462d4ea685c55d5251bfd65d1b8
Adrian <adrian@mxlinux.org>
2018-11-16 20:32:32 -0500

    don't add root entry in fstab if encrypted, not needed I think
    Former-commit-id: 01d2b1960c4c5c9da0d163b0fcf17b452cdb109a

--- minstall.cpp
+++ minstall.cpp
@@ -2797,7 +2797,9 @@ void MInstall::copyDone(int, QProcess::ExitStatus exitStatus)
         if (file.open(QIODevice::WriteOnly)) {
             QTextStream out(&file);
             out << "# Pluggable devices are handled by uDev, they are not in fstab\n";
-            out << rootdev + " / " + fstype + " " + root_mntops + " " + dump_pass + "\n";
+            if(!isRootEncrypted) {
+                out << rootdev + " / " + fstype + " " + root_mntops + " " + dump_pass + "\n";
+            }
             //add bootdev if present
             //only ext4 (for now) for max compatibility with other linuxes
             if (!bootdev.isEmpty()){
