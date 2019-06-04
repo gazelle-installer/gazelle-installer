25b0f78a6de1ae52c4440d224bcb4079bde55fee
AK-47 <chkboom>
2019-06-04 01:44:29 +0000

    Write directly to /mnt/antiX/var/log/minstall.conf instead of using cp. Also some preparations for automatic installation.

--- minstall.cpp
+++ minstall.cpp
@@ -31,8 +31,9 @@ MInstall::MInstall(const QStringList &args)
     setupUi(this);
     proc = new QProcess(this);
 
-    nocopy = (args.contains("--nocopy") || args.contains("-n"));
     pretend = (args.contains("--pretend") || args.contains("-p"));
+    automatic = (args.contains("--auto") || args.contains("-a"));
+    nocopy = (args.contains("--nocopy") || args.contains("-n"));
     sync = (args.contains("--sync") || args.contains("-s"));
     if (pretend) listHomes = args; // dummy existing homes
 
@@ -621,91 +622,90 @@ void MInstall::removeItemCombo(QComboBox *cb, const QString *part)
 void MInstall::saveConfig()
 {
     if (phase < 0) return;
+    QSettings confout("/mnt/antiX/var/log/minstall.conf", QSettings::NativeFormat);
     // Disk step
-    config->beginGroup("Disk");
-    config->setValue("Disk", diskCombo->currentText().section(" ", 0, 0));
-    config->setValue("Encrypted", checkBoxEncryptAuto->isChecked());
-    config->setValue("EntireDisk", entireDiskButton->isChecked());
-    config->endGroup();
+    confout.beginGroup("Disk");
+    confout.setValue("Disk", diskCombo->currentText().section(" ", 0, 0));
+    confout.setValue("Encrypted", checkBoxEncryptAuto->isChecked());
+    confout.setValue("EntireDisk", entireDiskButton->isChecked());
+    confout.endGroup();
     // Partition step
-    config->beginGroup("Partition");
+    confout.beginGroup("Partition");
 
-    config->setValue("Root", rootCombo->currentText().section(" ", 0, 0));
-    config->setValue("Home", homeCombo->currentText().section(" ", 0, 0));
-    config->setValue("Swap", swapCombo->currentText().section(" ", 0, 0));
-    config->setValue("Boot", bootCombo->currentText().section(" ", 0, 0));
+    confout.setValue("Root", rootCombo->currentText().section(" ", 0, 0));
+    confout.setValue("Home", homeCombo->currentText().section(" ", 0, 0));
+    confout.setValue("Swap", swapCombo->currentText().section(" ", 0, 0));
+    confout.setValue("Boot", bootCombo->currentText().section(" ", 0, 0));
 
-    config->setValue("RootType", rootTypeCombo->currentText());
-    config->setValue("HomeType", homeTypeCombo->currentText());
+    confout.setValue("RootType", rootTypeCombo->currentText());
+    confout.setValue("HomeType", homeTypeCombo->currentText());
 
-    config->setValue("RootEncrypt", checkBoxEncryptRoot->isChecked());
-    config->setValue("RootEncrypt", checkBoxEncryptHome->isChecked());
-    config->setValue("RootEncrypt", checkBoxEncryptSwap->isChecked());
+    confout.setValue("RootEncrypt", checkBoxEncryptRoot->isChecked());
+    confout.setValue("RootEncrypt", checkBoxEncryptHome->isChecked());
+    confout.setValue("RootEncrypt", checkBoxEncryptSwap->isChecked());
 
-    config->setValue("RootLabel", rootLabelEdit->text());
-    config->setValue("HomeLabel", homeLabelEdit->text());
-    config->setValue("SwapLabel", swapLabelEdit->text());
+    confout.setValue("RootLabel", rootLabelEdit->text());
+    confout.setValue("HomeLabel", homeLabelEdit->text());
+    confout.setValue("SwapLabel", swapLabelEdit->text());
 
-    config->setValue("SaveHome", saveHomeCheck->isChecked());
-    config->setValue("BadBlocksCheck", badblocksCheck->isChecked());
+    confout.setValue("SaveHome", saveHomeCheck->isChecked());
+    confout.setValue("BadBlocksCheck", badblocksCheck->isChecked());
 
-    config->endGroup();
+    confout.endGroup();
     // AES step
-    config->beginGroup("Encryption");
-    config->setValue("Cipher", comboFDEcipher->currentText());
-    config->setValue("ChainMode", comboFDEchain->currentText());
-    config->setValue("IVgenerator", comboFDEivgen->currentText());
-    config->setValue("IVhash", comboFDEivhash->currentData().toString());
-    config->setValue("KeySize", spinFDEkeysize->cleanText());
-    config->setValue("LUKSkeyHash", comboFDEhash->currentText().toLower().remove('-'));
-    config->setValue("KernelRNG", comboFDErandom->currentText());
-    config->setValue("KDFroundTime", spinFDEroundtime->cleanText());
-    config->endGroup();
+    confout.beginGroup("Encryption");
+    confout.setValue("Cipher", comboFDEcipher->currentText());
+    confout.setValue("ChainMode", comboFDEchain->currentText());
+    confout.setValue("IVgenerator", comboFDEivgen->currentText());
+    confout.setValue("IVhash", comboFDEivhash->currentData().toString());
+    confout.setValue("KeySize", spinFDEkeysize->cleanText());
+    confout.setValue("LUKSkeyHash", comboFDEhash->currentText().toLower().remove('-'));
+    confout.setValue("KernelRNG", comboFDErandom->currentText());
+    confout.setValue("KDFroundTime", spinFDEroundtime->cleanText());
+    confout.endGroup();
     // GRUB step
-    config->beginGroup("GRUB");
+    confout.beginGroup("GRUB");
     if(grubCheckBox->isChecked()) {
         const char *cfgGrubInstall;
         if(grubMbrButton->isChecked()) cfgGrubInstall = "MBR";
         if(grubPbrButton->isChecked()) cfgGrubInstall = "PBR";
         if(grubEspButton->isChecked()) cfgGrubInstall = "ESP";
-        config->setValue("InstallGRUB", cfgGrubInstall);
-        config->setValue("GrubLocation", grubBootCombo->currentText().section(" ", 0, 0));
+        confout.setValue("InstallGRUB", cfgGrubInstall);
+        confout.setValue("GrubLocation", grubBootCombo->currentText().section(" ", 0, 0));
     } else {
-        config->setValue("InstallGRUB", false);
+        confout.setValue("InstallGRUB", false);
     }
-    config->endGroup();
+    confout.endGroup();
     // Services step
-    config->beginGroup("Services");
+    confout.beginGroup("Services");
     QTreeWidgetItemIterator it(csView);
     while (*it) {
         if ((*it)->parent() != NULL) {
-            config->setValue((*it)->text(0), (*it)->checkState(0));
+            confout.setValue((*it)->text(0), (*it)->checkState(0));
         }
         ++it;
     }
-    config->endGroup();
+    confout.endGroup();
     // Network step
-    config->beginGroup("Network");
-    config->setValue("ComputerName", computerNameEdit->text());
-    config->setValue("Domain", computerDomainEdit->text());
-    config->setValue("Workgroup", computerGroupEdit->text());
-    config->setValue("Samba", sambaCheckBox->isChecked());
-    config->endGroup();
+    confout.beginGroup("Network");
+    confout.setValue("ComputerName", computerNameEdit->text());
+    confout.setValue("Domain", computerDomainEdit->text());
+    confout.setValue("Workgroup", computerGroupEdit->text());
+    confout.setValue("Samba", sambaCheckBox->isChecked());
+    confout.endGroup();
     // Localization step
-    config->beginGroup("Localization");
-    config->setValue("Locale", localeCombo->currentData().toString());
-    config->setValue("LocalClock", localClockCheckBox->isChecked());
-    config->setValue("Clock24h", radio24h->isChecked());
-    config->setValue("Timezone", timezoneCombo->currentText());
-    config->endGroup();
+    confout.beginGroup("Localization");
+    confout.setValue("Locale", localeCombo->currentData().toString());
+    confout.setValue("LocalClock", localClockCheckBox->isChecked());
+    confout.setValue("Clock24h", radio24h->isChecked());
+    confout.setValue("Timezone", timezoneCombo->currentText());
+    confout.endGroup();
     // User Accounts step
-    config->beginGroup("User");
-    config->setValue("Username", userNameEdit->text());
-    config->setValue("Autologin", autologinCheckBox->isChecked());
-    config->setValue("SaveDesktop", saveDesktopCheckBox->isChecked());
-    config->endGroup();
-    // copy config file to installed system
-    execute("cp \"" + config->fileName() + "\" /mnt/antiX/var/log", true);
+    confout.beginGroup("User");
+    confout.setValue("Username", userNameEdit->text());
+    confout.setValue("Autologin", autologinCheckBox->isChecked());
+    confout.setValue("SaveDesktop", saveDesktopCheckBox->isChecked());
+    confout.endGroup();
 }
 
 // update partition combos
