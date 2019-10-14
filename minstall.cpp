b65bc14ceac7aab4ecaf39004a4e72434d6a944a
Dolphin Oracle <dolphinoracle@gmail.com>
2019-10-14 14:44:23 -0400

    added sleeps
    Former-commit-id: 5e6828eb8d3061b9d7de048d96323cd537761047 [formerly 4bbad9d74aa6d2d4619af31c2ed5d1e888f3d32d]
    Former-commit-id: 87ae6cb857307556eb6222ffd7168f02640d1706

--- minstall.cpp
+++ minstall.cpp
@@ -283,6 +283,7 @@ void MInstall::setupAutoMount(bool enabled)
             proc.exec("rm -f /run/udev/rules.d/91-mx-udisks-inhibit.rules");
             proc.exec("udevadm control --reload");
             proc.exec("partprobe -s");
+            proc.exec("sleep 1");
         }
         // clear the rules that were temporarily overridden
         for (const QString &rule : udev_temp_mdadm_rules) {
@@ -867,6 +868,7 @@ bool MInstall::formatPartitions()
         updateStatus(tr("Formatting EFI System Partition"));
         if (!proc.exec("mkfs.msdos -F 32 " + espDevice)) return false;
         proc.exec("parted -s " + BlockDeviceInfo::split(espDevice).at(0) + " set 1 esp on"); // sets boot flag and esp flag
+        proc.exec("sleep 1");
     }
     // maybe format home
     if (homeFormatSize) {
@@ -951,6 +953,7 @@ bool MInstall::makeLinuxPartition(const QString &dev, const QString &type, bool 
         // ext4 tuning
         proc.exec("/sbin/tune2fs -c0 -C0 -i1m " + dev);
     }
+    proc.exec("sleep 1");
     return true;
 }
 
@@ -979,6 +982,7 @@ bool MInstall::makeLuksPartition(const QString &dev, const QByteArray &password)
         failUI(tr("Sorry, could not create %1 LUKS partition").arg(dev));
         return false;
     }
+    proc.exec("sleep 1");
     return true;
 }
 
@@ -1317,6 +1321,7 @@ bool MInstall::makePartitions()
             }
             rc = proc.exec(cmd.arg(devsplit.at(0), devsplit.at(1)));
         }
+        proc.exec("sleep 1");
         return rc;
     };
 
@@ -1337,6 +1342,7 @@ bool MInstall::makePartitions()
     } else {
         updateStatus(tr("Preparing required partitions"));
     }
+    proc.exec("sleep 1");
 
     // any new partitions they will appear in this order on the disk
     if (!biosGrubDevice.isEmpty()) {
@@ -1348,6 +1354,7 @@ bool MInstall::makePartitions()
     if (!lambdaPreparePart(homeDevice, homeFormatSize, "primary")) return false;
     if (!lambdaPreparePart(swapDevice, swapFormatSize, "primary")) return false;
     proc.exec("partprobe -s", true);
+    proc.exec("sleep 1");
     return true;
 }
 
@@ -1660,6 +1667,8 @@ bool MInstall::installLoader()
         }
         if (bootflag) proc.exec("parted -s " + boot + " set " + part_num + bootflag, true);
     }
+
+    proc.exec("sleep 1");
 
     // set mounts for chroot
     proc.exec("/bin/mount -o bind /dev /mnt/antiX/dev", true);
