78fadcf32c42a43375c72add1c0753c05b1ad714
Dolphin Oracle <dolphinoracle@gmail.com>
2020-01-08 21:24:14 -0500

    adjust find command to old return hidden files (including those in .config)

--- minstall.cpp
+++ minstall.cpp
@@ -3491,7 +3491,7 @@ void MInstall::rsynchomefolder(QString dpath)
 
 void MInstall::changeRemasterdemoToNewUser(QString dpath)
 {
-    QString cmd = ("find " + dpath + " -type f -print0 | xargs -0 sed -i 's|home/demo|home/" + userNameEdit->text() + "|g'").arg(dpath);
+    QString cmd = ("find " + dpath + " -type f -name '.*' -print0 | xargs -0 sed -i 's|home/demo|home/" + userNameEdit->text() + "|g'").arg(dpath);
     proc.exec(cmd);
 }
 
