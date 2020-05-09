d35c78962d74e0e0a213ac0b6646e83dcdc691fa
AK-47 <AK-47>
2020-05-09 04:18:55 +0000

    Move process UI setup into MProcess class.

--- minstall.cpp
+++ minstall.cpp
@@ -34,12 +34,7 @@ MInstall::MInstall(const QStringList &args, const QString &cfgfile)
     : proc(this)
 {
     setupUi(this);
-    QPalette palLog = listLog->palette();
-    palLog.setColor(QPalette::Base, Qt::black);
-    palLog.setColor(QPalette::Text, Qt::white);
-    listLog->setPalette(palLog);
-    proc.logView = listLog;
-    proc.progressBar = progressBar;
+    proc.setupUI(listLog, progressBar);
     updateCursor(Qt::WaitCursor);
     setWindowFlags(Qt::Window); // for the close, min and max buttons
     installBox->hide();
