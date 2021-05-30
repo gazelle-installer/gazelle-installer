fc7269237ea315429548be111430519b55d098cd
AK-47 <AK-47>
2021-05-30 03:47:15 +0000

    MLineEdit -> MPassEdit

--- translations/gazelle-installer_pl.ts
+++ translations/gazelle-installer_pl.ts
@@ -1020,9 +1020,9 @@ Czy naprawdę chcesz teraz zakończyć?</translation>
     </message>
 </context>
 <context>
-    <name>MLineEdit</name>
+    <name>MPassEdit</name>
     <message>
-        <location filename="mlineedit.cpp" line="95"/>
+        <location filename="mpassedit.cpp" line="95"/>
         <source>Use password</source>
         <translation type="unfinished"/>
     </message>
