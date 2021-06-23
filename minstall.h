b7fe3b87d6b41936a49aedcd302a37a14f357cd5
Adrian <adrian@mxlinux.org>
2021-06-23 08:31:52 -0400

    Style: changed copyright notices to use multiline comments
    Former-commit-id: 6b0b590de4767b798e1529c83e2db0e12aba6f37 [formerly fcff0c9cb8cc6d0d6d8834b13a6e82e9a564c243]
    Former-commit-id: 573598b1f9438c8355c4e6b728e6ba1d13a9f26b

--- minstall.h
+++ minstall.h
@@ -1,19 +1,21 @@
-//
-//   Copyright (C) 2003-2009 by Warren Woodford
-//   Heavily edited, with permision, by anticapitalista for antiX 2011-2014.
-//   Heavily revised by dolphin oracle, adrian, and anticaptialista 2018.
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
-//
+/****************************************************************************
+ *  Copyright (C) 2003-2010 by Warren Woodford
+ *  Heavily edited, with permision, by anticapitalista for antiX 2011-2014.
+ *  Heavily revised by dolphin oracle, adrian, and anticaptialista 2018.
+ *  additional mount and compression oftions for btrfs by rob 2018
+ *  Major GUI update and user experience improvements by AK-47 2019.
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ ****************************************************************************/
 
 #include <QCommandLineParser>
 #include <QFile>
