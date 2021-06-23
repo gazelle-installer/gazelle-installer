8cef5fcf0da98f9cf52fa851692215e4bfa6c742
Adrian <adrian@mxlinux.org>
2021-06-23 07:53:07 -0400

    Multiline comment for copyright notice, helps with code folding
    Former-commit-id: 1241af1b05778c3325a23093f93a6bf87ae6db6d

--- mpassedit.h
+++ mpassedit.h
@@ -1,19 +1,21 @@
-// MPassEdit class - QLineEdit modified for passwords.
-//
-//   Copyright (C) 2021 by AK-47
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
-//
-// This file is part of the gazelle-installer.
+/**********************************************************************
+ * MPassEdit class - QLineEdit modified for passwords.
+ **********************************************************************
+ *   Copyright (C) 2021 by AK-47
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ * This file is part of the gazelle-installer.
+ *********************************************************************/
 
 #ifndef MPASSEDIT_H
 #define MPASSEDIT_H
