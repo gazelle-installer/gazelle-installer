fe183ae28f3a70a631c674149ad919f2faa973cf
Adrian <adrian@mxlinux.org>
2021-06-23 08:31:52 -0400

    Style: changed copyright notices to use multiline comments
    Former-commit-id: 46b358c776a568b700216635048a7bee930a94ed

--- app.cpp
+++ app.cpp
@@ -1,19 +1,19 @@
-//
-//   Copyright (C) 2003-2009 by Warren Woodford
-//   Heavily edited, with permision, by anticapitalista for antiX 2011-2014.
-//   Heavily revised by dolphin oracle, adrian, and anticaptialista 2018.
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
-//
+/****************************************************************************
+ *   Copyright (C) 2003-2009 by Warren Woodford
+ *   Heavily edited, with permision, by anticapitalista for antiX 2011-2014.
+ *   Heavily revised by dolphin oracle, adrian, and anticaptialista 2018.
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ ***************************************************************************/
 
 #include <cstdio>
 #include <unistd.h>
--- partman.cpp
+++ partman.cpp
@@ -1,23 +1,26 @@
-// Basic partition manager for the installer.
-//
-//   Copyright (C) 2020-2021 by AK-47
-//   Transplanted code, marked with comments further down this file:
-//    - Copyright (C) 2003-2010 by Warren Woodford
-//    - Heavily edited, with permision, by anticapitalista for antiX 2011-2014.
-//    - Heavily revised by dolphin oracle, adrian, and anticaptialista 2018.
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
-//
-// This file is part of the gazelle-installer.
+/***************************************************************************
+ * Basic partition manager for the installer.
+ ***************************************************************************
+ *
+ *   Copyright (C) 2020-2021 by AK-47
+ *   Transplanted code, marked with comments further down this file:
+ *    - Copyright (C) 2003-2010 by Warren Woodford
+ *    - Heavily edited, with permision, by anticapitalista for antiX 2011-2014.
+ *    - Heavily revised by dolphin oracle, adrian, and anticaptialista 2018.
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ * This file is part of the gazelle-installer.
+ ***************************************************************************/
 
 #include <sys/sysinfo.h>
 #include <sys/stat.h>
