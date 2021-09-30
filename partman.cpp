a2acc26c4958777e4a57fbca0cd6fd5cc795049f
AK-47 <AK-47>
2021-09-30 10:24:13 +0000

    Fixed: combo box drop-down list gets duplicated when an entry is chosen. Move some spin box setup code into DeviceItemDelegate::createEditor().

--- partman.cpp
+++ partman.cpp
@@ -2096,7 +2096,16 @@ QWidget *DeviceItemDelegate::createEditor(QWidget *parent, const QStyleOptionVie
     QComboBox *combo = nullptr;
     switch (index.column())
     {
-    case PartMan::Size: widget = new QSpinBox(parent); break;
+    case PartMan::Size:
+        {
+            QSpinBox *spin = new QSpinBox(parent);
+            spin->setSuffix("MB");
+            spin->setStepType(QSpinBox::AdaptiveDecimalStepType);
+            spin->setAccelerated(true);
+            spin->setWrapping(true);
+            widget = spin;
+        }
+        break;
     case PartMan::UseFor:
         widget = combo = new QComboBox(parent);
         combo->setEditable(true);
@@ -2135,16 +2144,13 @@ void DeviceItemDelegate::setEditorData(QWidget *editor, const QModelIndex &index
         {
             QSpinBox *spin = qobject_cast<QSpinBox *>(editor);
             spin->setRange(1, static_cast<int>(item->driveFreeSpace() / 1048576));
-            spin->setSuffix("MB");
-            spin->setStepType(QSpinBox::AdaptiveDecimalStepType);
-            spin->setAccelerated(true);
             spin->setValue(item->size / 1048576);
-            spin->setWrapping(true);
         }
         break;
     case PartMan::UseFor:
         {
             QComboBox *combo = qobject_cast<QComboBox *>(editor);
+            combo->clear();
             combo->addItem("");
             combo->addItems(item->allowedUsesFor(false));
             combo->setCurrentText(item->usefor);
@@ -2156,6 +2162,7 @@ void DeviceItemDelegate::setEditorData(QWidget *editor, const QModelIndex &index
     case PartMan::Format:
         {
             QComboBox *combo = qobject_cast<QComboBox *>(editor);
+            combo->clear();
             const QStringList &formats = item->allowedFormats();
             assert(!formats.isEmpty());
             for (const QString &fmt : formats) {
