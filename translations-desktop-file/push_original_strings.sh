754392cb36705914ddf952516758bcc4e83a145e
Dolphin Oracle <dolphinoracle@gmail.com>
2022-12-20 20:03:33 -0500

    update push_original_strings so transifex config folder is made
    Former-commit-id: 48bdf4a334ee88f00c5680710b76f475a9657d47

--- translations-desktop-file/push_original_strings.sh
+++ translations-desktop-file/push_original_strings.sh
@@ -6,7 +6,24 @@
 #there is a confirmation step
 #and the resource must exist
 
+
 RESOURCE="$(basename $(dirname $(pwd)))-desktop"
+APPNAME="$(basename $(dirname $(pwd)))-desktop"
+
+# prepare transifex 
+if [ ! -s  .tx/config ]; then
+   mkdir -p .tx
+   cat <<EOF > .tx/config
+[main]
+host = https://www.transifex.com
+
+[antix-development.$APPNAME]
+file_filter = po/${APPNAME}_<lang>.po
+source_file = ${APPNAME}.pot
+source_lang = en
+type = po
+EOF
+fi    
 
 tx push -r antix-development.${RESOURCE} --translations --force
 
