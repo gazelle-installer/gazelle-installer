2d602aa0328a1a1b9ebe17428de284a152330dea
AK-47 <AK-47>
2023-07-01 03:20:30 +0000

    Refactored partition validation/confirmation method. Use rich text for the disk space warning. Factor btrfs subvolumes and other mounts in space calculations. Fixed disk space warning with unselected BIOS-GRUB partition. Fixed unbootable setup warning with existing BIOS-GRUB partition. Fixed: changing from encrypted volume to un-encryptable will encrypt it anyway. Added assert() where a DeviceItem pointer must never be null.
    Former-commit-id: be6b7bbee1fa92caba0427489d373f4b1eda9ad1

--- partman.h
+++ partman.h
@@ -182,6 +182,8 @@ class PartMan : public QAbstractItemModel
     void partMenuUnlock(DeviceItem *twit);
     void partMenuLock(DeviceItem *twit);
     void scanSubvolumes(DeviceItem *partit);
+    bool confirmSpace(class QMessageBox &msgbox) noexcept;
+    bool confirmBootable(class QMessageBox &msgbox) noexcept;
     void luksFormat(DeviceItem *partit, const QByteArray &password);
     void luksOpen(DeviceItem *partit, const QString &luksfs, const QByteArray &password);
 public:
