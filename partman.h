1e40c91e65bd0d63bcbf0f2553132d49adf89349
AK-47 <AK-47>
2023-07-19 11:29:39 +0000

    Move the Device class declaration out of the PartMan body to improve readability.

--- partman.h
+++ partman.h
@@ -43,93 +43,7 @@ class PartMan : public QAbstractItemModel
 {
     Q_OBJECT
 public:
-    class Device
-    {
-        Q_DECLARE_TR_FUNCTIONS(Device)
-    public:
-        Device *active = nullptr;
-        Device *origin = nullptr;
-        enum DeviceType {
-            UNKNOWN,
-            DRIVE,
-            PARTITION,
-            VIRTUAL_DEVICES,
-            VIRTUAL,
-            SUBVOLUME
-        } type = UNKNOWN;
-        struct Flags {
-            bool rotational : 1;
-            bool nasty : 1;
-            bool curEmpty : 1;
-            bool oldLayout : 1;
-            bool bootRoot : 1;
-            bool curMBR : 1;
-            bool curGPT : 1;
-            bool curESP : 1;
-            bool volCrypto : 1;
-            bool sysEFI : 1;
-        } flags = {};
-        QString model, name, path, uuid, map;
-        QString label, curLabel;
-        QString usefor;
-        QString format, curFormat;
-        QString options;
-        long long size = 0;
-        int mapCount = 0;
-        int physec;
-        int pass = 0;
-        int discgran = 0;
-        bool encrypt = false;
-        bool chkbadblk = false;
-        bool dump = false;
-        bool addToCrypttab = false;
-
-        Device(enum DeviceType type, Device *parent, Device *preceding = nullptr) noexcept;
-        ~Device();
-        void clear() noexcept;
-        int row() const noexcept;
-        Device *parent() const noexcept;
-        Device *child(int row) const noexcept;
-        int indexOfChild(const Device *device) const noexcept;
-        void sortChildren() noexcept;
-        // Layout finishing
-        Device *addPart(long long defaultSize, const QString &defaultUse, bool crypto) noexcept;
-        void driveAutoSetActive() noexcept;
-        void labelParts() noexcept;
-        // Helpers
-        void setActive(bool on) noexcept;
-        bool isActive() const noexcept;
-        bool isLocked() const noexcept;
-        bool willUseGPT() const noexcept;
-        bool willFormat() const noexcept;
-        bool canEncrypt() const noexcept;
-        bool willEncrypt() const noexcept;
-        QString assocUUID() const noexcept;
-        QString mappedDevice() const noexcept;
-        bool willMap() const noexcept;
-        QString shownDevice() const noexcept;
-        QStringList allowedUsesFor(bool all = true) const noexcept;
-        QStringList allowedFormats() const noexcept;
-        QString finalFormat() const noexcept;
-        QString shownFormat(const QString &fmt) const noexcept;
-        inline QString shownFormat() const noexcept { return shownFormat(format); }
-        inline bool isVolume() const noexcept { return (type == PARTITION || type == VIRTUAL); }
-        bool canMount(bool pointonly = true) const noexcept;
-        long long driveFreeSpace(bool inclusive = false) const noexcept;
-        /* Convenience */
-        void addToCombo(QComboBox *combo, bool warnNasty = false) const noexcept;
-
-    private:
-        friend class PartMan;
-        friend class Iterator;
-        friend class ItemDelegate;
-        std::vector<Device *> children;
-        Device *parentItem = nullptr;
-        class PartMan &partman;
-        int order = -1;
-        inline Device(class PartMan &pman) : partman(pman) {}
-        void autoFill(unsigned int changed = 0xFFFF) noexcept;
-    };
+    class Device;
     class Iterator;
     enum TreeColumns {
         COL_DEVICE,
@@ -157,6 +71,7 @@ public:
     class AutoPart *autopart = nullptr;
     PartMan(class MProcess &mproc, class Ui::MeInstall &ui,
         const class QSettings &appConf, const QCommandLineParser &appArgs);
+    ~PartMan();
     void scan(Device *drvstart = nullptr);
     bool manageConfig(class MSettings &config, bool save) noexcept;
     bool composeValidate(bool automatic, const QString &project) noexcept;
@@ -185,8 +100,8 @@ private:
     friend class Device;
     friend class Iterator;
     class MProcess &proc;
-    Device root;
-    Device *changing = nullptr;
+    class Device *root = nullptr;
+    class Device *changing = nullptr;
     Ui::MeInstall &gui;
     bool brave, gptoverride;
     bool goodluks; // TODO: Eliminate when MX Boot Repair is fixed.
@@ -232,6 +147,94 @@ public:
     bool changeBegin(Device *device) noexcept;
     int changeEnd(bool notify = true) noexcept;
     void notifyChange(class Device *device, int first = -1, int last = -1) noexcept;
+};
+
+class PartMan::Device
+{
+    Q_DECLARE_TR_FUNCTIONS(Device)
+public:
+    Device *active = nullptr;
+    Device *origin = nullptr;
+    enum DeviceType {
+        UNKNOWN,
+        DRIVE,
+        PARTITION,
+        VIRTUAL_DEVICES,
+        VIRTUAL,
+        SUBVOLUME
+    } type = UNKNOWN;
+    struct Flags {
+        bool rotational : 1;
+        bool nasty : 1;
+        bool curEmpty : 1;
+        bool oldLayout : 1;
+        bool bootRoot : 1;
+        bool curMBR : 1;
+        bool curGPT : 1;
+        bool curESP : 1;
+        bool volCrypto : 1;
+        bool sysEFI : 1;
+    } flags = {};
+    QString model, name, path, uuid, map;
+    QString label, curLabel;
+    QString usefor;
+    QString format, curFormat;
+    QString options;
+    long long size = 0;
+    int mapCount = 0;
+    int physec;
+    int pass = 0;
+    int discgran = 0;
+    bool encrypt = false;
+    bool chkbadblk = false;
+    bool dump = false;
+    bool addToCrypttab = false;
+
+    Device(enum DeviceType type, Device *parent, Device *preceding = nullptr) noexcept;
+    ~Device();
+    void clear() noexcept;
+    int row() const noexcept;
+    Device *parent() const noexcept;
+    Device *child(int row) const noexcept;
+    int indexOfChild(const Device *device) const noexcept;
+    void sortChildren() noexcept;
+    // Layout finishing
+    Device *addPart(long long defaultSize, const QString &defaultUse, bool crypto) noexcept;
+    void driveAutoSetActive() noexcept;
+    void labelParts() noexcept;
+    // Helpers
+    void setActive(bool on) noexcept;
+    bool isActive() const noexcept;
+    bool isLocked() const noexcept;
+    bool willUseGPT() const noexcept;
+    bool willFormat() const noexcept;
+    bool canEncrypt() const noexcept;
+    bool willEncrypt() const noexcept;
+    QString assocUUID() const noexcept;
+    QString mappedDevice() const noexcept;
+    bool willMap() const noexcept;
+    QString shownDevice() const noexcept;
+    QStringList allowedUsesFor(bool all = true) const noexcept;
+    QStringList allowedFormats() const noexcept;
+    QString finalFormat() const noexcept;
+    QString shownFormat(const QString &fmt) const noexcept;
+    inline QString shownFormat() const noexcept { return shownFormat(format); }
+    inline bool isVolume() const noexcept { return (type == PARTITION || type == VIRTUAL); }
+    bool canMount(bool pointonly = true) const noexcept;
+    long long driveFreeSpace(bool inclusive = false) const noexcept;
+    /* Convenience */
+    void addToCombo(QComboBox *combo, bool warnNasty = false) const noexcept;
+
+private:
+    friend class PartMan;
+    friend class Iterator;
+    friend class ItemDelegate;
+    std::vector<Device *> children;
+    Device *parentItem = nullptr;
+    class PartMan &partman;
+    int order = -1;
+    inline Device(class PartMan &pman) : partman(pman) {}
+    void autoFill(unsigned int changed = 0xFFFF) noexcept;
 };
 
 class PartMan::Iterator
