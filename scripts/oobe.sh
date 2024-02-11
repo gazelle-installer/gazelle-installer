cf0d6d439938978a8911d5855986cd5c2f71a838
Dolphin Oracle <dolphinoracle@gmail.com>
2024-02-10 22:48:42 -0500

    deal with nonexistent LANG
    Former-commit-id: c29280c2a8a6c1d08204a0b06046656fa1d21e1d

--- scripts/oobe.sh
+++ scripts/oobe.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
+#fallback C
+LANG=C
+
 #run mx-locale if it exists, in language chooser mode
 if [ -x "/usr/bin/mx-locale" ]; then
     /usr/bin/mx-locale --only-lang 2>/dev/null
 fi
 
-##source locale if it exists
+##source locale if it exists, get real LANG
 if [ -e "/etc/default/locale" ]; then
 	. /etc/default/locale
 fi
 
 #now run localized oobe
-
-if [ "$LANG" = '' ]; then
-	LANG=C
-fi
 
 LC_ALL=$LANG /usr/sbin/minstall --oobe --config 2>/dev/null
 
