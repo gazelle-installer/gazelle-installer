40afeea1edf4f806f42535f9bbafb445e9915936
AK-47 <AK-47>
2025-03-20 08:18:59 +0000

    Take advantage of btrfs/mkswap file creation features.

--- swapman.cpp
+++ swapman.cpp
@@ -91,17 +91,12 @@ void SwapMan::install(QStringList &cmdboot_out)
     // Create a blank swap file.
     proc.mkpath(QFileInfo(instpath).path(), 0700);
     const bool btrfs = (device->type == PartMan::Device::SUBVOLUME || device->finalFormat() == "btrfs");
+    const QString &strsize = QStringLiteral("%1M").arg(size);
     if (btrfs) {
-        proc.exec("truncate", {"--size=0", instpath});
-        proc.exec("chattr", {"+C", instpath});
+        proc.exec("btrfs", {"filesystem", "mkswapfile", "--size", strsize, instpath});
+    } else {
+        proc.exec("mkswap", {"-q", "--size", strsize, "--file", instpath});
     }
-#if 0 // TODO: Use for trixie
-    proc.exec("mkswap", {"-q", "-U","clear", "--size",QStringLiteral("%1M").arg(size), "--file", instpath});
-#else
-    proc.exec("dd", {"status=none", "if=/dev/zero", "of="+instpath, "bs=1M", QStringLiteral("count=%1").arg(size)});
-    chmod(instpath.toUtf8().constData(), 0600);
-    proc.exec("mkswap", {"-q", instpath});
-#endif
 
     proc.status(tr("Configuring swap file"));
     // Append the fstab with the swap file entry.
