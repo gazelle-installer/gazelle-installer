ada81601bf1b9e9295813257c7ba95454f4b722b
AK-47 <AK-47>
2025-11-03 10:01:54 +0000

    Improve throbber code readability.

--- minstall.cpp
+++ minstall.cpp
@@ -1081,32 +1081,37 @@ bool MInstall::eventFilter(QObject *watched, QEvent *event) noexcept
     if (event->type() != QEvent::Paint) {
         return QDialog::eventFilter(watched, event);
     } else if (watched == gui.labelSplash) {
-        // Setup needed to draw the load indicator.
+        // Draw the load indicator on the splash screen.
         QPainter painter(gui.labelSplash);
         painter.setRenderHints(QPainter::Antialiasing);
         const int lW = gui.labelSplash->width(), lH = gui.labelSplash->height();
         painter.translate(lW / 2, lH / 2);
         painter.scale(lW / 200.0, lH / 200.0);
-        // Draw the load indicator on the splash screen.
+
         static constexpr int blades = 12;
         static constexpr qreal angle = 360.0 / blades;
-        painter.rotate(angle * throbPos);
-        float hue = 1.0, alpha = 0.18;
-        const float huestep = std::min(throbPos, 360) / (360.0 * blades);
         static constexpr float alphastep = 0.18 / blades;
+        const float huestep = std::min(throbPos, 360) / (360.0 * blades);
+
+        float hue = 1.0, alpha = 0.18;
         QPen pen;
         pen.setWidth(3);
         pen.setJoinStyle(Qt::MiterJoin);
+
+        painter.rotate(angle * throbPos); // Angle of current position.
         for (unsigned int ixi=blades; ixi>0; --ixi) {
-            const QColor &color = QColor::fromHsvF(hue, 1.0, 1.0, alpha);
-            hue -= huestep, alpha += alphastep;
+            static constexpr QPoint blade[] = {{-15, -6}, {15, -75}, {0, -93}, {-15, -75}};
             static constexpr qreal bladeangle = 23.5; // atan(30/(75-6)) in degrees.
+
             QConicalGradient gradient(-15, -6, 90-bladeangle);
+            const QColor &color = QColor::fromHsvF(hue, 1.0, 1.0, alpha);
             gradient.setStops({{0, color}, {bladeangle/360.0, color.darker()}});
+            hue -= huestep, alpha += alphastep;
+
             painter.setBrush(gradient);
             pen.setColor(color.darker());
             painter.setPen(pen);
-            static constexpr QPoint blade[] = {{-15, -6}, {15, -75}, {0, -93}, {-15, -75}};
+
             painter.drawConvexPolygon(blade, 4);
             painter.rotate(angle);
         }
