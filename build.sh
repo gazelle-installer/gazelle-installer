23fcb8426ac652525eb5ad6113fd48748e7e7509
Adrian <adrian@mxlinux.org>
2025-12-23 00:34:11 -0500

    Replace 'arch' command with --arch option for Arch package building

--- build.sh
+++ build.sh
@@ -3,14 +3,12 @@
 set -e
 
 # Parse options
-PRESET="default"
-ARCH_MODE=false
+ARCH_PACKAGE=false
 
 while [[ $# -gt 0 ]]; do
 	case $1 in
 		--arch)
-			PRESET="arch"
-			ARCH_MODE=true
+			ARCH_PACKAGE=true
 			shift
 			;;
 		*)
@@ -18,6 +16,16 @@ while [[ $# -gt 0 ]]; do
 			;;
 	esac
 done
+
+# Handle --arch option (package build)
+if [ "$ARCH_PACKAGE" = true ]; then
+	echo "Building Arch Linux package..."
+	rm -rf _build_/arch
+	mkdir -p _build_/arch
+	PKGDEST="$(pwd)/_build_/arch" BUILDDIR="$(pwd)/_build_/arch" makepkg -f --cleanbuild --syncdeps --noconfirm --needed
+	echo "Package (and build artifacts) are in _build_/arch"
+	exit 0
+fi
 
 case "${1:-all}" in
 	clean)
@@ -27,47 +35,38 @@ case "${1:-all}" in
 
 	configure)
 		echo "Configuring project..."
-		cmake --preset $PRESET
+		cmake --preset default
 		;;
 
 	make-clean)
 		echo "Cleaning build artifacts..."
-		cmake --build --preset $PRESET --target clean
+		cmake --build --preset default --target clean
 		;;
 
 	make)
 		echo "Building project..."
-		cmake --build --preset $PRESET
-		;;
-
-	arch)
-		echo "Building Arch Linux package..."
-		rm -rf _build_/arch
-		mkdir -p _build_/arch
-		PKGDEST="$(pwd)/_build_/arch" BUILDDIR="$(pwd)/_build_/arch" makepkg -f --cleanbuild --syncdeps --noconfirm --needed
-		echo "Package (and build artifacts) are in _build_/arch"
+		cmake --build --preset default
 		;;
 
 	all)
 		echo "Configuring and building project..."
-		cmake --workflow --preset $PRESET
+		cmake --workflow --preset default
 		;;
 
 	fresh)
 		echo "Fresh build (clean first, then configure and build)..."
-		cmake --workflow --preset $PRESET --fresh
+		cmake --workflow --preset default --fresh
 		;;
 
 	*)
 		echo "Usage: $0 [--arch] [command]"
 		echo "Options:"
-		echo "  --arch        Use Arch Linux preset (disables zxcvbn)"
+		echo "  --arch        Build Arch Linux package using makepkg"
 		echo "Commands:"
 		echo "  clean        - Ultimate clean (rm -rf build)"
 		echo "  configure    - Configure only"
 		echo "  make-clean   - Clean build artifacts only"
 		echo "  make         - Build only"
-		echo "  arch         - Build using the Arch preset and create an Arch package"
 		echo "  all          - Configure and build (default)"
 		echo "  fresh        - Clean first then configure and build"
 		exit 1
